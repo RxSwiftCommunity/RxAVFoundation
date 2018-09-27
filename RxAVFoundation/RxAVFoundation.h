c6b8d1f969519cf9ba2a58ea5a2757d55aa029f6
Maxim Volgin <maxim.volgin@me.com>
2018-09-27 07:59:30 +0200

    initial

---
+++ RxAVFoundation/RxAVFoundation.h
@@ -0,0 +1,19 @@
+//
+//  RxAVFoundation.h
+//  RxAVFoundation
+//
+//  Created by Maxim Volgin on 27/09/2018.
+//  Copyright © 2018 Maxim Volgin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxAVFoundation.
+FOUNDATION_EXPORT double RxAVFoundationVersionNumber;
+
+//! Project version string for RxAVFoundation.
+FOUNDATION_EXPORT const unsigned char RxAVFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAVFoundation/PublicHeader.h>
+
+
