74d6b5ac1088d8ceee8dd52d37b527c5459fbd3b
Maxim Volgin <maxvol@users.noreply.github.com>
2019-06-24 03:59:01 +0200

    Create Package.swift
    SPM support

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "RxAVFoundation",
+    products: [
+        .library(
+            name: "RxAVFoundation",
+            targets: ["RxAVFoundation"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.1")
+    ],
+    targets: [
+        .target(
+            name: "RxAVFoundation",
+            dependencies: ["RxSwift"])
+    ]
+)
