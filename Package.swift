5c5a8a1a85fb238350310e82de5c6bc7f513704a
Maxim Volgin <maxvol@users.noreply.github.com>
2019-06-24 03:59:49 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "RxAVFoundation",
-            dependencies: ["RxSwift"])
+            dependencies: ["RxSwift"],
+            path: "RxAVFoundation")
     ]
 )
